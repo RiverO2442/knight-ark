79ac3e4c3c96a22dd855c2baef310b8faf9a68e0
rivero <nguyem.anhhao.840@gmail.com>
2023-12-06 22:02:01 +0700

    init

---
+++ src/main/java/com/rhodeisland/org/KnightArkApplication.java
@@ -0,0 +1,14 @@
+package com.rhodeisland.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnightArkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnightArkApplication.class, args);
+	}
+
+
+}
